cad7b9c0fe608808129705a521a29924ff614f9a
jimmy-phmt <Jimjim64>
2019-12-05 17:35:01 +0100

    first commit

---
+++ src/main/java/miage/jpho/bowling/App.java
@@ -0,0 +1,13 @@
+package miage.jpho.bowling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
